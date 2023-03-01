cfa738359c4f6672473b9d6474ba21b7edf88ba5
smileymcfrown <46418167+smileymcfrown@users.noreply.github.com>
2023-03-01 13:07:44 +1100

    First - NavMesh and Doors

---
+++ AI - Project 1/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public int WaitTime = 3;
+    public int Speed = 2;
+    public Vector3 PositionDelta = Vector3.zero;
+
+    private Vector3 _closedPosition;
+    private Vector3 _openPosition;
+
+    
+// Start is called before the first frame update
+    void Start()
+    {
+        _closedPosition = transform.position;
+        _openPosition = _closedPosition + PositionDelta;
+
+        Debug.Log("Before Coroutine");
+        StartCoroutine(OpenClose());
+        Debug.Log("After Coroutine");
+    }
+
+    IENumerator OpenClose()
+    {
+       Vector3 goal = _openPosition;
+       bool isOpen = false;
+       
+       While(true)
+       {
+            if(Vector3.Distance(transform.positon, goal) < 0.1f)
+            {
+                isOpen = !isOpen;
+                if(isOpen) { goal = _closedPosition; } else { goal = _openPosition; }
+                
+                yield return new WaitForSeconds(WaitTime);
+            }
+            else{}
+       }
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
