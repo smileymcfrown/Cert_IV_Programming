52094a85ab87cd96526f367384f964288e365d12
smileymcfrown <46418167+smileymcfrown@users.noreply.github.com>
2023-03-01 17:39:37 +1100

    Character Movement Controller

--- Game_Systems_Wk2/Assets/Scripts/Player/MouseLook.cs
+++ Game_Systems_Wk2/Assets/Scripts/Player/MouseLook.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    private enum RotationalAxis
+    {
+        MouseX,
+        MouseY
+    }
+    private RotationalAxis _axis = RotationalAxis.MouseX;
+    static public float sensitivity = 15f;
+    static public bool invertMouseY = false;
+    private Vector2 _clamp = new Vector2(-60f, 60f);
+
     // Start is called before the first frame update
     void Start()
     {
