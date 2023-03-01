34c48360f7374cfb57687211c20e99496a4d9ee9
smileymcfrown <46418167+smileymcfrown@users.noreply.github.com>
2023-03-01 17:50:22 +1100

    Need to add Update() mouse code

--- Game_Systems_Wk2/Assets/Scripts/Player/MouseLook.cs
+++ Game_Systems_Wk2/Assets/Scripts/Player/MouseLook.cs
@@ -9,15 +9,28 @@ public class MouseLook : MonoBehaviour
         MouseX,
         MouseY
     }
-    private RotationalAxis _axis = RotationalAxis.MouseX;
+    [SerializeField] private RotationalAxis _axis = RotationalAxis.MouseX;
     static public float sensitivity = 15f;
     static public bool invertMouseY = false;
     private Vector2 _clamp = new Vector2(-60f, 60f);
+    private float _rotationY;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (this.gameObject.tag == "Player")
+        {
+            _axis = RotationalAxis.MouseX;
+        }
+        else
+        {
+            _axis = RotationalAxis.MouseY;
+        }
+        //If there's a RigidBody then freeze the rotation so there is no conflict in movement
+        if (GetComponent<Rigidbody>())
+        {
+            GetComponent<Rigidbody>().freezeRotation = true;
+        }
     }
 
     // Update is called once per frame
